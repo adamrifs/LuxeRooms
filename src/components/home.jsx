86a4b8c77e140ba773db2b1a1fe1d18f8ad627c7
adamrifs <rifsadam81@gmail.com>
2024-06-18 17:27:50 +0530

    update

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -41,54 +41,54 @@ function Home() {
 
                 {/* =========== landing page ========= */}
                 <div className="m-box">
-                    <div className="m-sub-box">
 
-                        {
-                            furniture.map((dt) =>
-                                <>
-                                    <div className="m-b-left-box">
-                                        <div className="m-b-title">
-                                            {dt.title}
-                                        </div>
+                    {
+                        furniture.map((dt) =>
+                            <div className="m-sub-box">
 
-                                        <div className="mb-head">
-                                            <h1>{dt.heading}</h1>
-                                        </div>
-
-                                        <div className="mb-desc">
-                                            {dt.description}
-                                        </div>
-                                        <div className="mb-sizes">
-                                            <h5>Sizes</h5>
-                                            <input type="radio" id='black' name='size' />
-                                            <label for='black'>Black</label>
-
-                                            <input type="radio" id='wood' name='size' />
-                                            <label for='wood'>Wood</label>
-                                        </div>
-                                        <hr />
-                                        <div className="mb-cart-btn">
-                                            <button>Add to cart</button>
-                                        </div>
+                                <div className="m-b-left-box">
+                                    <div className="m-b-title">
+                                        {dt.title}
                                     </div>
-                                    <div className="mb-right-box">
 
-                                        <div className="mb-fr-name">
-                                            {dt.frname}
-                                        </div>
-
-                                        <div className="mb-light-img">
-                                            <img src={dt.lightimg} alt='light' />
-                                        </div>
-                                        <div className="mb-rt-furn-img">
-                                            <img src={dt.furimage} alt="furniture" />
-                                        </div>
+                                    <div className="mb-head">
+                                        <h1>{dt.heading}</h1>
                                     </div>
-                                </>
-                            )
-                        }
 
-                    </div>
+                                    <div className="mb-desc">
+                                        {dt.description}
+                                    </div>
+                                    <div className="mb-sizes">
+                                        <h5>Sizes</h5>
+                                        <input type="radio" id='black' name='size' />
+                                        <label for='black'>Black</label>
+
+                                        <input type="radio" id='wood' name='size' />
+                                        <label for='wood'>Wood</label>
+                                    </div>
+                                    <hr />
+                                    <div className="mb-cart-btn">
+                                        <button>Add to cart</button>
+                                    </div>
+                                </div>
+                                <div className="mb-right-box">
+
+                                    <div className="mb-fr-name">
+                                        {dt.frname}
+                                    </div>
+
+                                    <div className="mb-light-img">
+                                        <img src={dt.lightimg} alt='light' />
+                                    </div>
+                                    <div className="mb-rt-furn-img">
+                                        <img src={dt.furimage} alt="furniture" />
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    }
+
+
                 </div>
 
             </div>
